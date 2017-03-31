f75cb9de61b832a0d9b40a6fa04989ef33783b29
Chelsea Crain <chelsea.crain@gmail.com>
2017-03-31 12:34:59 -0500

    Create schedule_scrape.bat

---
+++ schedule_scrape.bat
@@ -0,0 +1,7 @@
+for /f %%x in ('wmic path win32_localtime get /format:list ^| findstr "="') do set %%x
+set today=%Month%.%Day%.%Year%
+
+echo %today%
+
+
+schtasks /create /sc once /ri 5 /tn "BoulderScrape"  /st 12:00 /tr "D:Boulder_Scrapes\start_direct.cmd  boulder %today% 
