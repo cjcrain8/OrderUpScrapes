0332224ff764d5b9efcabae8e4a7eeed7dc6d7ae
Chelsea Crain <chelsea.crain@gmail.com>
2017-03-31 12:43:30 -0500

    Update schedule_scrape.bat

--- schedule_scrape.bat
+++ schedule_scrape.bat
@@ -4,4 +4,4 @@ set today=%Month%.%Day%.%Year%
 echo %today%
 
 
-schtasks /create /sc once /ri 5 /tn "BoulderScrape"  /st 12:00 /tr "D:Boulder_Scrapes\start_direct.cmd  boulder %today% 
+schtasks /create /sc once /ri 5 /tn "BoulderScrape"  /st 12:00 /DU 24:00 /tr "D:Boulder_Scrapes\start_direct.cmd  boulder %today% 
